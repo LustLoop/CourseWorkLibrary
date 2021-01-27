536cafeec10c764f749fd1100b88a111b94e6328
Volodymyr Yesypenko <volodymyryesypenko@gmail.com>
2021-01-27 03:26:29 +0200

    Fix typo

--- src/services/books.js
+++ src/services/books.js
@@ -1,7 +1,7 @@
 const Book = require('../db/schemes/book');
 
-const addBook = (title, description, rating, author, genre, availability) => {
-    return new Book({title, description, rating, author, genre, availability}).save();
+const addBook = (title, description, rating, author, genre, available) => {
+    return new Book({title, description, rating, author, genre, available}).save();
 }
 
 const getBooks = () => {
